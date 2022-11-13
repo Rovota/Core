84f28d1fa3a75090e147f93762d8e588ebd25bb4
Léandro Tijink <leandro.tijink@gmail.com>
2022-11-13 11:04:17 +0100

    added: missing copyright notice

--- src/Support/Structures/Sequence.php
+++ src/Support/Structures/Sequence.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * @author      Software Department <developers@rovota.com>
+ * @copyright   Copyright (c), Rovota
+ * @license     Rovota License
+ */
+
+
 namespace Rovota\Core\Support\Structures;
 
 use TypeError;
