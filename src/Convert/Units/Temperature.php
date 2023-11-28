6de58be2610e4850855a26cbd362c82f47eba74b
Léandro Tijink <leandro.tijink@gmail.com>
2023-11-28 19:56:25 +0100

    removed: unnecessary class imports

--- src/Convert/Units/Temperature.php
+++ src/Convert/Units/Temperature.php
@@ -9,19 +9,6 @@
 namespace Rovota\Core\Convert\Units;
 
 use Rovota\Core\Convert\Enums\UnitType;
-use Rovota\Core\Convert\Units\Length\CentiMeter;
-use Rovota\Core\Convert\Units\Length\DeciMeter;
-use Rovota\Core\Convert\Units\Length\Foot;
-use Rovota\Core\Convert\Units\Length\HectoMeter;
-use Rovota\Core\Convert\Units\Length\Inch;
-use Rovota\Core\Convert\Units\Length\KiloMeter;
-use Rovota\Core\Convert\Units\Length\Meter;
-use Rovota\Core\Convert\Units\Length\MicroMeter;
-use Rovota\Core\Convert\Units\Length\Mile;
-use Rovota\Core\Convert\Units\Length\MilliMeter;
-use Rovota\Core\Convert\Units\Length\NanoMeter;
-use Rovota\Core\Convert\Units\Length\NauticalMile;
-use Rovota\Core\Convert\Units\Length\Yard;
 use Rovota\Core\Convert\Units\Temperature\Celsius;
 use Rovota\Core\Convert\Units\Temperature\Fahrenheit;
 use Rovota\Core\Convert\Units\Temperature\Kelvin;
