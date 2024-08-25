1a750b1249220027ff84f4f19a0870389d72bd1b
Léandro Tijink <leandro.tijink@gmail.com>
2024-08-25 22:27:20 +0200

    removed: client and bot code for requests

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -13,78 +13,20 @@ use Rovota\Core\Http\Traits\RequestInput;
 use Rovota\Core\Routing\Route as RouteObject;
 use Rovota\Core\Structures\ErrorBucket;
 use Rovota\Core\Support\Traits\Errors;
-use Rovota\Core\Validation\Enums\FilterAction;
-use Rovota\Core\Validation\FilterManager;
 use Rovota\Core\Validation\Traits\RequestValidation;
 
 final class Request
 {
 	use RequestInput, RequestValidation, Errors;
 
-	protected array $client_names;
-	protected array $client_robots;
-
 	// -----------------
 
 	public function __construct(string|null $body, array $post, array $query, array $headers)
 	{
-		$this->client_names = include base_path('vendor/rovota/core/src/Http/client_names.php');
-		$this->client_robots = include base_path('vendor/rovota/core/src/Http/client_robots.php');
-
 		$this->errors = new ErrorBucket();
 	}
 
-	// -----------------
-
-	/**
-	 * Uses the experimental 'Sec-CH-UA' HTTP header.
-	 * For non-supported clients, it will guess the client name.
-	 */
-	public function client(string|null $default = null): string|null
-	{
-		$client = null;
-		if ($this->hasHeader('Sec-CH-UA')) {
-			$names = array_reduce(explode(',', trim($this->header('Sec-CH-UA'))),
-				function ($carry, $element) {
-					$brand = Str::remove(Str::beforeLast($element, ';'), '"');
-					$version = str_contains($element, ';v=') ? Str::afterLast($element, ';v=') : '';
-					if (Str::containsNone($brand, ['Brand', 'Chromium'])) {
-						$carry[trim($brand)] = (int) Str::remove($version, '"');
-					}
-					return $carry;
-				},[]
-			);
-			$client = array_key_first($names);
-		}
-
-		if ($client !== null) {
-			return $client;
-		}
-
-		foreach ($this->client_names as $client => $name) {
-			if (str_contains($this->header('User-Agent', ''), $client)) {
-				return $name;
-			}
-		}
-		return $default;
-	}
-
-	public function ipAllowed(string|null $ip = null): bool
-	{
-		$filter = FilterManager::get('access_control');
-		if ($filter !== null && $filter->action === FilterAction::Block) {
-			return Arr::contains($filter->values, $ip ?? $this->ip()) === false;
-		}
-		if ($filter !== null && $filter->action === FilterAction::Allow) {
-			return Arr::contains($filter->values, $ip ?? $this->ip());
-		}
-		return true;
-	}
-
-	public function isBot(): bool
-	{
-		return text($this->header('User-Agent', ''))->lower()->containsAny($this->client_robots);
-	}
+	// ---------------
 
 	public function route(): RouteObject|null
 	{
--- src/Http/Traits/RequestInput.php
+++ src/Http/Traits/RequestInput.php
@@ -8,38 +8,12 @@
 
 namespace Rovota\Core\Http\Traits;
 
-use Rovota\Core\Facades\Session;
-use Rovota\Core\Http\RequestData;
 use Rovota\Core\Http\UploadedFile;
 
 trait RequestInput
 {
-
-	public readonly string|null $body;
 	public readonly RequestData $post;
 	public readonly RequestData $query;
-
-	// -----------------
-
-	public function keep(): void
-	{
-		Session::flashMany($this->getInputData()->all());
-	}
-
-	public function keepOnly(string|array $keys): void
-	{
-		Session::flashMany($this->getInputData()->only($keys)->all());
-	}
-
-	public function keepExcept(string|array $keys): void
-	{
-		Session::flashMany($this->getInputData()->except($keys)->all());
-	}
-
-	public function old(string $key, mixed $default = null): mixed
-	{
-		return Session::pull($key, $default);
-	}
 
 	// -----------------
 
