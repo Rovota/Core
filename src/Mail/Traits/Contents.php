9c01ff63e0a5ad8ea2f17cc5a4ccd640e66185c5
Léandro Tijink <leandro.tijink@gmail.com>
2025-01-12 21:39:42 +0100

    removed: most of mailing implementation

--- src/Mail/Traits/Contents.php
+++ src/Mail/Traits/Contents.php
@@ -8,70 +8,13 @@
 
 namespace Rovota\Core\Mail\Traits;
 
-use League\Flysystem\FilesystemException;
-use Rovota\Core\Mail\Enums\Encoding;
 use Rovota\Core\Storage\Interfaces\FileInterface;
 use Rovota\Core\Storage\StorageManager;
-use Rovota\Core\Support\Str;
 use Rovota\Core\Support\ValidationTools;
-use Rovota\Core\Views\View;
-use Rovota\Core\Views\ViewManager;
 
 trait Contents
 {
-
-	private string|null $subject = null;
-	private string|null $plain = null;
-	private string|null $text = null;
-
-	private View|array|null $view = null;
-
-	private array $variables = [];
 	private array $attachments = [];
-
-	// -----------------
-
-	public function subject(string $content): static
-	{
-		$this->subject = trim($content);
-		return $this;
-	}
-
-	public function summary(string $content): static
-	{
-		$this->variables['mail_summary'] = $content;
-		return $this;
-	}
-
-	// -----------------
-
-	public function plain(string $content): static
-	{
-		$this->plain = trim($content);
-		return $this;
-	}
-
-	public function view(View|string $name, string|null $source = null): static
-	{
-		if ($name instanceof View) {
-			$this->view = $name;
-		} else {
-			$this->view = ['name' => $name,'source' => $source];
-		}
-		return $this;
-	}
-
-	public function text(string $content): static
-	{
-		$this->text = trim($content);
-		return $this;
-	}
-
-	public function with(string $name, mixed $data): static
-	{
-		$this->variables[$name] = $data;
-		return $this;
-	}
 
 	// -----------------
 
@@ -107,38 +50,6 @@ trait Contents
 		];
 
 		return $this;
-	}
-
-	// -----------------
-
-	public function render(): string|null
-	{
-		$content = null;
-		$view = null;
-
-		if ($this->plain !== null) {
-			$content = $this->plain;
-			$this->mailer->isHTML(false);
-			foreach ($this->variables as $name => $value) {
-				$content = str_replace(sprintf('{%% %s %%}', $name), $value, $content);
-			}
-		}
-
-		if (is_array($this->view)) {
-			$view = ViewManager::makeMail($this->view['name'], $this->view['source'], $this->language);
-		} else if ($this->view instanceof View) {
-			$view = $this->view;
-		}
-
-		if ($view instanceof View) {
-			$this->mailer->isHTML();
-			foreach ($this->variables as $name => $value) {
-				$view->with($name, $value);
-			}
-			$content = $view->render();
-		}
-
-		return $content;
 	}
 
 }
