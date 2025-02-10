92862c4e8e884842254c06806ef1598d5c1cb823
Léandro Tijink <leandro.tijink@gmail.com>
2025-02-10 14:19:39 +0100

    removed: redundant files

--- src/Auth/AccessManager.php
+++ src/Auth/AccessManager.php
@@ -8,32 +8,10 @@
 
 namespace Rovota\Core\Auth;
 
-use Rovota\Core\Cookie\Cookie;
-use Rovota\Core\Cookie\CookieManager;
-use Rovota\Core\Facades\Registry;
 use Rovota\Core\Http\RequestManager;
-use Rovota\Core\Kernel\ExceptionHandler;
-use Rovota\Core\Structures\Bucket;
-use Rovota\Core\Support\Enums\Status;
-use Rovota\Core\Support\Str;
-use Throwable;
 
 final class AccessManager
 {
-
-	/**
-	 * @var Bucket<int, Permission>
-	 */
-	protected static Bucket $permissions;
-
-	protected static array $permission_map = [];
-
-	/**
-	 * @var Bucket<int, Role>
-	 */
-	protected static Bucket $roles;
-
-	protected static array $role_map = [];
 
 	protected static string $csrf_token;
 
@@ -54,102 +32,6 @@ final class AccessManager
 		self::$roles = new Bucket();
 
 		self::initializeCsrfToken();
-	}
-
-	// -----------------
-	// Permissions
-
-	public static function loadPermission(string|int $identifier): void
-	{
-		try {
-			$permission = Permission::where(['status' => Status::Enabled])->find($identifier, is_numeric($identifier) ? 'id' : 'name');
-			if ($permission instanceof Permission) {
-				self::$permissions[$permission->id] = $permission;
-				self::$permission_map[$permission->name] = $permission->id;
-			}
-		} catch (Throwable $throwable) {
-			ExceptionHandler::addThrowable($throwable, true);
-		}
-	}
-
-	public static function rememberPermission(Permission $permission): void
-	{
-		self::$permissions[$permission->id] = $permission;
-		self::$permission_map[$permission->name] = $permission->id;
-	}
-
-	public static function knowsPermission(string|int $identifier): bool
-	{
-		return self::findPermissionByIdentifier($identifier) instanceof Permission;
-	}
-
-	public static function getPermission(string|int $identifier): Permission|null
-	{
-		return self::findPermissionByIdentifier($identifier);
-	}
-
-	public static function getOrLoadPermission(string|int $identifier): Permission|null
-	{
-		if (self::knowsPermission($identifier) === false) {
-			self::loadPermission($identifier);
-		}
-		return self::findPermissionByIdentifier($identifier);
-	}
-
-	/**
-	 * @returns Bucket<int, Permission>
-	 */
-	public static function getPermissions(): Bucket
-	{
-		return self::$permissions;
-	}
-
-	// -----------------
-	// Roles
-
-	public static function loadRole(string|int $identifier): void
-	{
-		try {
-			$role = Role::where(['status' => Status::Enabled])->find($identifier, is_numeric($identifier) ? 'id' : 'name');
-			if ($role instanceof Role) {
-				self::$roles[$role->id] = $role;
-				self::$role_map[$role->name] = $role->id;
-			}
-		} catch (Throwable $throwable) {
-			ExceptionHandler::addThrowable($throwable, true);
-		}
-	}
-
-	public static function rememberRole(Role $role): void
-	{
-		self::$roles[$role->id] = $role;
-		self::$role_map[$role->name] = $role->id;
-	}
-
-	public static function knowsRole(string|int $identifier): bool
-	{
-		return self::findRoleByIdentifier($identifier) instanceof Role;
-	}
-
-	public static function getRole(string|int $identifier): Role|null
-	{
-		return self::findRoleByIdentifier($identifier);
-	}
-
-	public static function getOrLoadRole(string|int $identifier): Role|null
-	{
-		if (self::knowsRole($identifier) === false) {
-			self::loadRole($identifier);
-		}
-		return self::findRoleByIdentifier($identifier);
-	}
-
-	/**
-	 * @returns Bucket<int, Role>
-	 */
-	public static function getRoles(): Bucket
-	{
-		return self::$roles;
 	}
 
 	// -----------------
@@ -175,22 +57,6 @@ final class AccessManager
 
 	// -----------------
 	// Internal
-
-	protected static function findPermissionByIdentifier(string|int $identifier): Permission|null
-	{
-		if (is_string($identifier) && isset(self::$permission_map[$identifier])) {
-			$identifier = self::$permission_map[$identifier];
-		}
-		return self::$permissions[$identifier] ?? null;
-	}
-
-	protected static function findRoleByIdentifier(string|int $identifier): Role|null
-	{
-		if (is_string($identifier) && isset(self::$role_map[$identifier])) {
-			$identifier = self::$role_map[$identifier];
-		}
-		return self::$roles[$identifier] ?? null;
-	}
 
 	protected static function initializeCsrfToken(): void
 	{
--- src/Auth/User.php
+++ src/Auth/User.php
@@ -9,18 +9,7 @@
 namespace Rovota\Core\Auth;
 
 use Rovota\Core\Auth\Interfaces\Identity;
-use Rovota\Core\Auth\Traits\Permissions;
-use Rovota\Core\Cache\CacheManager;
-use Rovota\Core\Database\Model;
-use Rovota\Core\Kernel\ExceptionHandler;
-use Rovota\Core\Localization\Language;
-use Rovota\Core\Localization\LocalizationManager;
-use Rovota\Core\Security\Hash;
-use Rovota\Core\Support\Enums\Status;
-use Rovota\Core\Support\Moment;
-use Rovota\Core\Support\Traits\Metadata;
 use Throwable;
-use function now;
 
 /**
  * @property int $id
@@ -33,8 +22,8 @@ use function now;
  * @property string $password
  * @property int $language_id
  * @property int $role_id
- * @property array|null $permission_list
- * @property array|null $permissions_denied
+ * @property array $permission_list
+ * @property array $permissions_denied
  * @property Moment|null $last_active
  * @property Status $status
  * @property Moment|null $created
@@ -43,35 +32,8 @@ use function now;
  */
 class User extends Model implements Identity
 {
-	use Permissions, Metadata;
-
-	protected string|null $table = 'users';
-
-	protected array $attributes = [
-		'email_verified' => false,
-		'role_id' => 1,
-	];
-
-	protected array $casts = [
-		'email_verified' => 'bool',
-		'permission_list' => 'array',
-		'permissions_denied' => 'array',
-		'last_active' => 'moment',
-		'status' => ['enum', Status::class],
-	];
-
-	protected array $guarded = ['id', 'created', 'edited', 'deleted'];
-
-	protected bool $enable_composites = false;
-
-	protected string $meta_model = UserMeta::class;
-	protected string $meta_foreign_key = 'user_id';
-
-	// -----------------
 
 	public Suspension|null $suspension = null;
-	public Language|null $language = null;
-	public Role|null $role = null;
 
 	protected array $twofactor_methods = [];
 	protected string|null $twofactor_default = null;
@@ -80,52 +42,8 @@ class User extends Model implements Identity
 
 	public function eventModelLoaded(): void
 	{
-		$this->loadLanguage();
 		$this->loadSuspension();
-		$this->loadPermissions();
-		$this->loadRole();
-		$this->loadMeta();
 		$this->loadTwoFactorMethods();
-	}
-
-	// -----------------
-	// Generic
-
-	public function getName(): string
-	{
-		return $this->username;
-	}
-
-	public function getDisplayName(): string
-	{
-		return $this->nickname;
-	}
-
-	public function getEmail(): string
-	{
-		return $this->email;
-	}
-
-	// -----------------
-	// Language
-
-	public function setLanguage(Language|string|int $identifier, bool $save = false): void
-	{
-		if (is_string($identifier) || is_int($identifier)) {
-			$identifier = LocalizationManager::getLanguage($identifier);
-		}
-
-		$this->language = $identifier;
-		$this->language_id = $identifier->id;
-
-		if ($save) {
-			$this->save();
-		}
-	}
-
-	public function getLanguage(): Language|null
-	{
-		return $this->language;
 	}
 
 	// -----------------
@@ -147,79 +65,6 @@ class User extends Model implements Identity
 	public function getSuspension(): Suspension|null
 	{
 		return $this->suspension;
-	}
-
-	// -----------------
-	// Role
-
-	public function setRole(Role|string|int $identifier, bool $save = false): void
-	{
-		if (is_string($identifier) || is_int($identifier)) {
-			$identifier = AccessManager::getOrLoadRole($identifier);
-		}
-
-		$this->role = $identifier;
-		$this->role_id = $identifier->id;
-
-		if ($save) {
-			$this->save();
-		}
-	}
-
-	public function hasRole(array|string|int $identifiers): bool
-	{
-		if ($this->role === null) {
-			return false;
-		}
-
-		$identifiers = is_array($identifiers) ? $identifiers : [$identifiers];
-		foreach ($identifiers as $identifier) {
-			if (is_int($identifier) ? $this->role->id === $identifier: $this->role->name === $identifier) {
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	public function getRole(): Role|null
-	{
-		return $this->role;
-	}
-
-	// -----------------
-	// Last Active
-
-	public function updateLastActive(): void
-	{
-		$key = 'last_active_hit:'.$this->getId();
-		if (CacheManager::get()->has($key) === false) {
-			$this->last_active = now();
-			$this->save();
-			CacheManager::get()->set($key, now()->toDateTimeString(), 300);
-		}
-	}
-
-	public function getLastActive(): Moment
-	{
-		$key = 'last_active_hit:'.$this->getId();
-		return CacheManager::get()->get($key, $this->created);
-	}
-
-	public function isOnline(int $minutes = 5): bool
-	{
-		if ($this->getLastActive()->diffInMinutes() <= $minutes) {
-			return true;
-		}
-		return false;
-	}
-
-	public function isActive(int $days = 365): bool
-	{
-		if ($this->getLastActive()->diffInDays() <= $days) {
-			return true;
-		}
-		return false;
 	}
 
 	// -----------------
@@ -300,20 +145,6 @@ class User extends Model implements Identity
 		} catch (Throwable $throwable) {
 			ExceptionHandler::addThrowable($throwable);
 		}
-	}
-
-	protected function loadLanguage(): void
-	{
-		if (LocalizationManager::hasLanguage($this->language_id)) {
-			$this->language = LocalizationManager::getLanguage($this->language_id);
-		} else {
-			$this->language = LocalizationManager::getActiveLanguage();
-		}
-	}
-
-	protected function loadRole(): void
-	{
-		$this->role = AccessManager::getOrLoadRole($this->role_id);
 	}
 
 	protected function loadTwoFactorMethods(): void
